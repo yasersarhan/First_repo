f714af5e0478e9f837bac870d0c1470a5fca939c
yaser sarhan <73104662+yasersarhan@users.noreply.github.com>
2020-10-19 21:36:09 +0200

    add more functions

---
+++ functions.py
@@ -0,0 +1,23 @@
+def cube(num):
+    return num * num * num
+
+
+result = cube(8)
+print(result)
+
+
+def yaser(num1, num2, num3):
+    return num1 + num2 + num3
+
+
+result = yaser(79, 93, 94)
+print(result)
+
+
+def calc(num4, num5, num6, num7):
+    return (num4 + num5) * num6 / num7
+    print("yaser sarhan")
+
+
+total = calc(10, 40, 2, 50)
+print(total)
