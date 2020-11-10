54cb9a48ee22ee2a8aa77bf2530eb2d7979a326e
yaser sarhan <73104662+yasersarhan@users.noreply.github.com>
2020-11-10 09:54:49 +0200

    create class iphone for example

---
+++ OOP.py
@@ -0,0 +1,11 @@
+class Iphone():
+
+    def open_camera(self):
+        print('opening camera')
+
+    def passlock(self):
+        print('trying to open')
+
+
+my_iphone = Iphone()
+my_iphone.open_camera()
