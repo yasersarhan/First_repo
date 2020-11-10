eea4b39d9b21a2f3329808dd628eef08de5b697e
yaser sarhan <73104662+yasersarhan@users.noreply.github.com>
2020-11-10 10:42:12 +0200

    what is self mean in python?

--- OOP.py
+++ OOP.py
@@ -1,14 +1,20 @@
 class Calculator():
 
     def sum (self,x,y):
-        print(x+y)
+        print(self)
+        #print(x+y)
 
     def mul (self,x,y):
         print(x*y)
 
 
 my_calc = Calculator()
+print(my_calc)
 my_calc.sum(4,5)
-my_calc.mul(3,8)
+#my_calc.mul(3,8)
+
+your_calc = Calculator()
+print(your_calc)
+your_calc.sum(8,9)
 
 
