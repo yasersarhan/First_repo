0a4bc0571ec7b15b071936131f2eb0a22c74dc1b
yaser sarhan <73104662+yasersarhan@users.noreply.github.com>
2020-11-10 13:02:30 +0200

    add constructor to class Calculator

--- OOP.py
+++ OOP.py
@@ -1,20 +1,19 @@
 class Calculator():
+    def __init__(self):
+        print('welcome in our calculator')
 
     def sum (self,x,y):
-        print(self)
-        #print(x+y)
+        print(x+y)
 
     def mul (self,x,y):
         print(x*y)
 
 
 my_calc = Calculator()
-print(my_calc)
+
 my_calc.sum(4,5)
-#my_calc.mul(3,8)
 
 your_calc = Calculator()
-print(your_calc)
-your_calc.sum(8,9)
+your_calc.mul(8,9)
 
 
