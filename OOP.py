edbd613a4f52fa8463792f9167e7709d5d4fcfd7
yaser sarhan <73104662+yasersarhan@users.noreply.github.com>
2020-11-10 14:27:09 +0200

    Example for multiple inheritance

--- OOP.py
+++ OOP.py
@@ -1,29 +1,20 @@
-class Calculator():
+class Main_calc():
     def __init__(self):
         print('welcome in our calculator')
-
     def sum (self,x,y):
         print(x+y)
+    
 
+class Calculator():
     def mul (self,x,y):
         print(x*y)
 
-
-my_calc = Calculator()
-my_calc.sum(4,5)
-
-class Scientific(Calculator):
+class Scientific(Calculator, Main_calc):
     def power(self,x,y):
        print(x**y)
 
-    def divison(slef,x,y):
-       print(x/y)
-
-    def sub(self,x,y):
-        print(x-y)
-
 x = Scientific()
-x.sum(1,1)
+x.sum(44,56)
 
     
 
