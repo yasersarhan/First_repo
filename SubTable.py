aa16bd02cd4e4e7b735ebd15ae36c303016828d2
yaser sarhan <73104662+yasersarhan@users.noreply.github.com>
2020-11-15 14:14:28 +0200

    create variables take word from uasers

--- SubTable.py
+++ SubTable.py
@@ -1,8 +1,15 @@
-start = int(input('Please enter the first num'))
-end = int(input('Please enter the end num'))
+'''
+start = int(input('Please enter the first num: '))
+end = int(input('Please enter the end num: '))
 
 for num in range(start, end+1):
    for value in range(1,13):
        result = num*value
        print(f'{value} x {num} = {result}')
    print('----------')
+'''
+word = input('Enter a word: ')
+letters = []
+for letter in word:
+    letters.append(letter)
+print(letters)
