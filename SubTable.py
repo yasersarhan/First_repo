5844f25a241b591b5e802677346d25aa7599edda
yaser sarhan <73104662+yasersarhan@users.noreply.github.com>
2020-11-15 14:06:31 +0200

    create 2 inputs to take value from users

--- SubTable.py
+++ SubTable.py
@@ -1,4 +1,7 @@
-for num in range(1,13):
+start = int(input('Please enter the first num'))
+end = int(input('Please enter the end num'))
+
+for num in range(start, end+1):
    for value in range(1,13):
        result = num*value
        print(f'{value} x {num} = {result}')
