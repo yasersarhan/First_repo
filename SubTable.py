79dc75a96a76c2302a59f1321774fa22cf5e0074
yaser sarhan <73104662+yasersarhan@users.noreply.github.com>
2020-11-15 15:25:05 +0200

    finish the game

--- SubTable.py
+++ SubTable.py
@@ -1,32 +1,43 @@
-'''
-start = int(input('Please enter the first num: '))
-end = int(input('Please enter the end num: '))
-
-for num in range(start, end+1):
-   for value in range(1,13):
-       result = num*value
-       print(f'{value} x {num} = {result}')
-   print('----------')
-'''
-'''
-word = input('Enter a word: ')
-letters = []
-for letter in word:
-    if letter in letters:
-        continue
-    else:
-        letters.append(letter)
-print(letters)
-'''
 class Game:
+
     def __init__(self):
-        
-        print('Welcom In Our Game')
+
+        print('Welcome In Our Game')
         print('''
             Press the game number:
                 1) Multiplication table.
-                2) Remove dublicated chars.
+                2) Remove duplicated chars.
                 ''')
+
         user_choice = int(input('Enter your choice number: '))
+
+        if user_choice == 1:
+            self.multiplacation_table()
+        elif user_choice == 2:
+            self.remove_duplicates()
+        else:
+            print('Please Enter A Valid Number')
+
+    def multiplacation_table(self):
+
+        start = int(input('Please enter the first num: '))
+        end = int(input('Please enter the end num: '))
+
+        for num in range(start, end + 1):
+            for value in range(1, 13):
+                result = num * value
+                print(f'{value} x {num} = {result}')
+            print('----------')
+
+    def remove_duplicates(self):
+        word = input('Enter a word: ')
+        letters = []
+        for letter in word:
+            if letter in letters:
+                continue
+            else:
+                letters.append(letter)
+        print(letters)
+
 
 g = Game()
