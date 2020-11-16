a415ca71643e2a7b75cb33f8e8829ec82fe9aaba
yasersarhan <sarhan.whitewhale@gmail.com>
2020-11-16 22:35:20 +0200

    Create while loop to continue play

--- First_Example.py
+++ First_Example.py
@@ -1,3 +1,9 @@
 Num = 0
-
-Num = int(input('Please Enter Your Number'))
+while Num >= 0:
+    Num = int(input('Please Enter Your Number: '))
+    if Num % 2 == 0:
+        print('This Is An Even Number')
+    else:
+        print('This Is An Odd Number')
+else:
+    print('Please Enter A Valid Number')
